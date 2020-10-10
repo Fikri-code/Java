0bb8db4a797336e7520142ce13287773582261d4
Muhamad Fikri Al-Baihaqi <35265185+Fikri-code@users.noreply.github.com>
2020-10-10 22:10:23 +0700

    Add files via upload

---
+++ Bitwise.java
@@ -0,0 +1,11 @@
+public class Bitwise { 
+    public static void main(String[] args) 
+    { 
+        //Initial values 
+        int a = 5; 
+        int b = 7; 
+
+        System.out.println("a&b = " + (a & b)); 
+        System.out.println("a|b = " + (a | b));
+    } 
+} 
