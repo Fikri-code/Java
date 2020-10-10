7313dd26ae2f9dbbbe5e8847e738274479daf733
Muhamad Fikri Al-Baihaqi <35265185+Fikri-code@users.noreply.github.com>
2020-10-10 22:45:28 +0700

    Add files via upload

---
+++ Concatenation.java
@@ -0,0 +1,8 @@
+public class Concatenation {
+    public static void main(String[] args) {
+      String greeting = "Hello";
+      String name = greeting + " Fikri";
+
+      System.out.println(name);
+    }
+}
