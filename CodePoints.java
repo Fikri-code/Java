7aa172561747104c85d4e4cd393ba73a20e7314a
Muhamad Fikri Al-Baihaqi <35265185+Fikri-code@users.noreply.github.com>
2020-10-10 23:06:59 +0700

    Add files via upload

---
+++ CodePoints.java
@@ -0,0 +1,10 @@
+public class CodePoints {
+    public static void main(String[] args) {
+      String greeting = "Hello";
+      int n = greeting.length();
+      char first = greeting.charAt(0);
+
+      System.out.println(first);
+    }
+    
+}
