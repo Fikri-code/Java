2ba3410252ae4c1437829f48230e562015b933ea
Muhamad Fikri Al-Baihaqi <35265185+Fikri-code@users.noreply.github.com>
2020-10-10 22:56:53 +0700

    Add files via upload

---
+++ StringEquality.java
@@ -0,0 +1,9 @@
+public class StringEquality {
+    public static void main(String[] args) {
+      String greeting = "Hello";
+      boolean x = "Hello".equals(greeting);
+
+      System.out.println(x);
+    }
+    
+}
