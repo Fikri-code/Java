26d66589502e9905a95fd3a6aa60bf95f992fe21
Muhamad Fikri Al-Baihaqi <35265185+Fikri-code@users.noreply.github.com>
2020-10-10 22:21:12 +0700

    Add files via upload

---
+++ Hierarcy.java
@@ -0,0 +1,10 @@
+public class Hierarcy { 
+    public static void main(String[] args) 
+    { 
+        int x = 8 + 9 * 3;
+        int y = (8 + 9) * 3;
+
+        System.out.println(x);
+        System.out.println(y);
+    } 
+} 
