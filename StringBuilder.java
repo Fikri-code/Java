e0c4586a7b555e65f4fa086d520714e7b8af7562
Muhamad Fikri Al-Baihaqi <35265185+Fikri-code@users.noreply.github.com>
2020-10-10 23:35:46 +0700

    Add files via upload

---
+++ StringBuilder.java
@@ -0,0 +1,8 @@
+public class StringBuilder {
+  public static void main(String[] args) {
+    StringBuilder builder = new StringBuilder("Hello ");
+
+    builder.append("World");
+    System.out.println(builder);
+  }
+}
