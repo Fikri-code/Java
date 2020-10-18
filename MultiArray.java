2183400611df6fb7ad3bf9341c2957d6b53161ba
Muhamad Fikri Al-Baihaqi <35265185+Fikri-code@users.noreply.github.com>
2020-10-18 23:03:42 +0700

    Add files via upload

---
+++ MultiArray.java
@@ -0,0 +1,13 @@
+public class MultiArray {
+  public static void main(String[] args) {
+    int[][] magicSquare =
+    {
+      {16, 3, 2, 13},
+      {5, 10, 11, 8},
+      {9, 6, 7, 12},
+      {4, 15, 14, 1}
+    };
+
+    System.out.println(magicSquare[0][3]);
+  }
+}
