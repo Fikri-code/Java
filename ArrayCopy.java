5061571fbe2a98aca66d71ae308c062a6a34022a
Muhamad Fikri Al-Baihaqi <35265185+Fikri-code@users.noreply.github.com>
2020-10-18 22:30:36 +0700

    Add files via upload

---
+++ ArrayCopy.java
@@ -0,0 +1,14 @@
+import java.util.Arrays;
+
+public class ArrayCopy {
+  public static void main(String[] args) {
+    int a[] = new int[]{10, 11, 12};
+    int b[] = Arrays.copyOf(a, a.length);
+    b[0] = 100;
+    a[1] = 7;
+    for(int x = 0; x<a.length;x++){
+      System.out.print("a : "+ a[x] + " | ");
+      System.out.print("b : "+ b[x] + " | ");
+    }
+  }
+}
